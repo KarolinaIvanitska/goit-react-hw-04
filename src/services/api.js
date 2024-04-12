0ae454abbe0743ed43cb667e896cdeaef69fab15
Karolina Zaiats <karolina.ivanitska@gmail.com>
2024-04-12 16:16:04 +0200

    api

--- src/services/api.js
+++ src/services/api.js
@@ -1,19 +1,21 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://api.unsplash.com/";
+const API_KEY = "bxCWOCG6Ryn-QR9Q9kaus_sxPIUu908puB1XN4bBNhg";
 
-export const fetchImages = async (query, page) => {
+axios.defaults.baseURL = "https://api.unsplash.com";
+
+export async function fetchImages(searchItem = "", page = 1) {
   try {
-    const response = await axios.get("search/photos", {
+    const response = await axios.get(`/search/photos`, {
       params: {
-        query,
-        page,
-        per_page: 15,
-        client_id: "fF4smEQdGwdsaSVAzGSHoukpDdVB9II8pe87g6f1gaE",
+        query: searchItem,
+        per_page: 12,
+        page: page,
+        client_id: API_KEY,
       },
     });
     return response.data;
-  } catch (error) {
-    console.log("Error!");
+  } catch (err) {
+    console.log(err);
   }
-};
+}
